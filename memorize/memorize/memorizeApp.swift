6b25160f7a53e6fbd7085057cc1c79ca5e6b53dc
Aaron Bruckner <aaronbruckner@aarons-mbp.lan>
2023-10-10 07:40:12 -0700

    Initial commit

---
+++ memorize/memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by Aaron Bruckner on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
