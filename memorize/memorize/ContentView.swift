947c82cf239e8597d690ab604c234cb07bb95d2b
Aaron Bruckner <aaronbruckner@aarons-mbp.lan>
2023-10-10 19:40:28 -0700

    Added some stuff, basic layout

--- memorize/memorize/ContentView.swift
+++ memorize/memorize/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis: [String] = ["🎃", "🕷️", "👻", "👹"]
+    @State var cardCount = 4
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                ForEach(0..<cardCount, id: \.self) { i in
+                    Card(content: emojis[i], isFaceUp: true)
+                }
+            }
+            HStack {
+                getCardAdjusterButton(adjustment: -1, iconSystemName: "rectangle.stack.badge.minus.fill")
+                    .disabled(cardCount == 1)
+                getCardAdjusterButton(adjustment: +1, iconSystemName: "rectangle.stack.fill.badge.plus")
+                    .disabled(cardCount == emojis.count)
+            }.font(.largeTitle)
         }
         .padding()
+    }
+    
+    func getCardAdjusterButton(adjustment: Int, iconSystemName: String) -> some View {
+        Button(action: {
+            cardCount += adjustment
+        }, label: {
+            Image(systemName: iconSystemName)
+        })
+    }
+}
+
+struct Card: View {
+    let content: String
+    @State var isFaceUp = true
+    
+    var body: some View {
+        ZStack{
+            let base = RoundedRectangle(cornerRadius: 12)
+            if isFaceUp {
+                base.foregroundColor(.white)
+                base.strokeBorder(lineWidth: 4)
+            } else {
+                base.fill()
+            }
+            Text(content).opacity(isFaceUp ? 1 : 0).font(.largeTitle)
+        }.onTapGesture {
+            isFaceUp.toggle()
+        }
     }
 }
 
