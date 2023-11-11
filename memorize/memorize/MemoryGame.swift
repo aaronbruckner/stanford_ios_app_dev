280d48cfe4bd8a7d1c2e0441bef54ac9766de5ec
Aaron Bruckner <aaronbruckner@gmail.com>
2023-11-11 08:41:22 -0800

    Implmented basic game logic that tracks flips and matches

--- memorize/memorize/MemoryGame.swift
+++ memorize/memorize/MemoryGame.swift
@@ -7,8 +7,9 @@
 
 import Foundation
 
-struct MemoryGame<CardContent> {
+struct MemoryGame<CardContent: Equatable> {
     private(set) var cards: [Card] = []
+    private var lastPickedCard: Card?
     
     init(pairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
         for pairIndex in 0 ..< pairsOfCards {
@@ -20,7 +21,7 @@ struct MemoryGame<CardContent> {
     
     struct Card: Identifiable {
         let id: String
-        var isFaceUp = true
+        var isFaceUp = false
         var isMatched = false
         let content: CardContent
         
@@ -35,10 +36,45 @@ struct MemoryGame<CardContent> {
     }
     
     mutating func chooseCard(_ card: Card) {
-        flipCardAtIndex(cards.firstIndex(where: {$0.id == card.id})!)
+        let selectedIndex = cards.firstIndex(where: {$0.id == card.id})!
+        
+        if (cards[selectedIndex].isFaceUp) {
+            // Do nothing if selected card is already face up.
+            return
+        }
+        
+        if let lastPickedCard = self.lastPickedCard {
+            let lastPickedIndex = self.cards.firstIndex(where: {$0.id == lastPickedCard.id})!
+            // User has made previous select and this is the second choice, see if the two selected cards match
+            checkIfPairMatches(index1: lastPickedIndex, index2: selectedIndex)
+            self.lastPickedCard = nil
+        } else {
+            // First choice in a pair
+            lastPickedCard = card
+            // Flip all other cards currently face up
+            for (i, card) in cards.enumerated() {
+                if card.isFaceUp && !card.isMatched {
+                    flipCardAtIndex(i)
+                }
+            }
+        }
+        flipCardAtIndex(selectedIndex)
     }
     
     private mutating func flipCardAtIndex(_ i: Int) {
         cards[i] = cards[i].flip()
+    }
+    
+    private mutating func checkIfPairMatches(index1: Int, index2: Int) {
+        if cards[index1].content == cards[index2].content {
+            markMatched(index1)
+            markMatched(index2)
+        }
+    }
+    
+    private mutating func markMatched(_ i: Int) {
+        var card = cards[i]
+        card.isMatched.toggle()
+        cards[i] = card
     }
 }
